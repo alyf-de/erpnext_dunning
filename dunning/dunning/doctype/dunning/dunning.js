bd587d9ebbc03a38cc00d67cf639efededd78967
ppereira1 <paul.pereira@alyf.de>
2019-07-05 17:50:33 +0100

    Add JS Fucnctions sum and diff

--- dunning/dunning/doctype/dunning/dunning.js
+++ dunning/dunning/doctype/dunning/dunning.js
@@ -2,7 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Dunning', {
-	refresh: function(frm) {
+	refresh: function (frm) {
+		
+	},
+	calculate_overdue_days: function(frm) {
+		const diff = moment(frm.doc.posting_date).diff(frm.doc.due_date,"days");
+		frm.set_value("overdue_days", diff);
+	},
+	due_date: function(frm) {
+		frm.trigger("calculate_overdue_days");
+	},
+	posting_date: function(frm) {
+		frm.trigger("calculate_overdue_days");
+	},
+	calculate_sum: function(frm){
+		const sum = frm.get_field("outstanding_amount").get_value() + frm.get_field("interest_amount").get_value() + frm.get_field("dunning_fee").get_value();
+		frm.set_value("sum", sum);
+	},
+	outstanding_amount: function(frm){
+		frm.trigger("calculate_sum");
+	},
+	interest_amount: function(frm){
+		frm.trigger("calculate_sum");
+	},
+	dunning_fee: function(frm){
+		frm.trigger("calculate_sum");
+	}	
 
-	}
-});
+});			
