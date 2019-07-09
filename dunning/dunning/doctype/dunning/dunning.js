645d7667a52a725753574bc39142d5fa9b025c95
Raffael Meyer <raffael@alyf.de>
2019-07-09 22:14:00 +0200

    refactoring

--- dunning/dunning/doctype/dunning/dunning.js
+++ dunning/dunning/doctype/dunning/dunning.js
@@ -3,11 +3,13 @@
 
 frappe.ui.form.on('Dunning', {
 	refresh: function (frm) {
-		
-	},
-	calculate_overdue_days: function(frm) {
-		const diff = moment(frm.doc.posting_date).diff(frm.doc.due_date,"days");
-		frm.set_value("overdue_days", diff);
+		frm.trigger("calculate_sum");
+		frm.trigger("calculate_overdue_days");
+		frm.trigger("calculate_interest");
+
+		if (frm.is_new()) {
+			frm.set_value("posting_date", frappe.datetime.nowdate());
+		}
 	},
 	due_date: function(frm) {
 		frm.trigger("calculate_overdue_days");
@@ -15,9 +17,8 @@ frappe.ui.form.on('Dunning', {
 	posting_date: function(frm) {
 		frm.trigger("calculate_overdue_days");
 	},
-	calculate_sum: function(frm){
-		const sum = frm.get_field("outstanding_amount").get_value() + frm.get_field("interest_amount").get_value() + frm.get_field("dunning_fee").get_value();
-		frm.set_value("sum", sum);
+	interest_rate: function (frm){
+		frm.trigger("calculate_interest");
 	},
 	outstanding_amount: function(frm){
 		frm.trigger("calculate_sum");
@@ -28,8 +29,28 @@ frappe.ui.form.on('Dunning', {
 	dunning_fee: function(frm){
 		frm.trigger("calculate_sum");
 	},
+	calculate_overdue_days: function(frm) {
+		const posting_date = frm.get_field("posting_date").get_value();
+		const due_date = frm.get_field("due_date").get_value();
+
+		const overdue_days = moment(posting_date).diff(due_date, "days");
+		frm.set_value("overdue_days", overdue_days);
+	},
+	calculate_sum: function(frm){
+		const outstanding_amount = frm.get_field("outstanding_amount").get_value() || 0;
+		const interest_amount = frm.get_field("interest_amount").get_value() || 0;
+		const dunning_fee = frm.get_field("dunning_fee").get_value() || 0;
+		
+		const sum = outstanding_amount + interest_amount + dunning_fee;
+		frm.set_value("sum", sum);
+	},
 	calculate_interest: function(frm) {
-		const interest = (frm.get_field("interest_rate").get_value() * frm.get_field("outstanding_amount").get_value() * frm.get_field("overdue_days").get_value()) / (100 * 365);
-		frm.set_value("interest_amount", interest);
+		const interest_rate = frm.get_field("interest_rate").get_value() || 0;
+		const outstanding_amount = frm.get_field("outstanding_amount").get_value() || 0;
+		const overdue_days = frm.get_field("overdue_days").get_value() || 0;
+
+		const interest_per_year = outstanding_amount * interest_rate / 100;
+		const interest_amount = interest_per_year / 360 * overdue_days;
+		frm.set_value("interest_amount", interest_amount);
 	}	
 });
