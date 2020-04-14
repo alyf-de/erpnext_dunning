622f594dbc5a4bbe51f002689a708efb9e2a9e59
Samuel Danieli <23150094+scdanieli@users.noreply.github.com>
2020-04-14 15:00:58 +0200

    feat: Added hook, so that dunnings get resolved automatically

--- dunning/dunning/doctype/dunning/dunning.py
+++ dunning/dunning/doctype/dunning/dunning.py
@@ -8,3 +8,13 @@ from frappe.model.document import Document
 
 class Dunning(Document):
 	pass
+
+def resolve_dunnings_for_payment_entry(doc, state):
+	for reference in doc.references:
+		if reference.reference_doctype == 'Sales Invoice' and reference.outstanding_amount <= 0:
+			dunnings = frappe.get_list('Dunning', filters={'sales_invoice': reference.reference_name, 'status': ('!=', 'Resolved')})
+			
+			for dunning in dunnings:
+				dunning_obj = frappe.get_doc('Dunning', dunning.name)
+				dunning_obj.status = 'Resolved'
+				dunning_obj.save()
--- dunning/hooks.py
+++ dunning/hooks.py
@@ -79,13 +79,11 @@ app_license = "MIT"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-# 	"*": {
-# 		"on_update": "method",
-# 		"on_cancel": "method",
-# 		"on_trash": "method"
-#	}
-# }
+doc_events = {
+	"Payment Entry": {
+		"on_submit": "dunning.dunning.doctype.dunning.dunning.resolve_dunnings_for_payment_entry"
+	}
+}
 
 # Scheduled Tasks
 # ---------------
